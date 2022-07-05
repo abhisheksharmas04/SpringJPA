4004f213b76ca69a2b84ab7af938722b0a1a7f48
DeLL <DeLL@DESKTOP-LOBR338>
2022-07-05 05:59:10 +0530

    Project Created

---
+++ BootDataJpaProj5-finderMethod-WithProjection/src/main/java/com/example/demo/BootDataJpaProj5FinderMethodWithProjectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDataJpaProj5FinderMethodWithProjectionApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Created");
+		SpringApplication.run(BootDataJpaProj5FinderMethodWithProjectionApplication.class, args);
+	}
+
+}
